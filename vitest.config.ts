d723b02376d236352925f2b8c8937b0a455c2a70
Diego Barreiro Perez <diego@barreiro.dev>
2026-01-31 20:26:07 +0000

    Fix Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,12 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
+        include: ["tests/**/*.test.ts"],
         environment: "jsdom",
+        coverage: {
+            provider: "v8",
+            reporter: ["text", "html", "lcov"],
+        },
     },
     resolve: {
         alias: {
